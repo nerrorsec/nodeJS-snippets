3899c821cd0a8fa66db46de2352976da2ff360e0
sushil phuyal <87110634+1337mickey@users.noreply.github.com>
2023-01-14 12:49:34 +0545

    testing child process for rat

--- rat.js
+++ rat.js
@@ -1,3 +1,7 @@
-<script>
-  storage.objects.get()
-</script>
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+require('child_process').exec('ls')
