c57f44d937ccb5d7cc69a1e168a8babf4564fbd8
sushil phuyal <87110634+1337mickey@users.noreply.github.com>
2023-01-14 12:51:48 +0545

    Create RAT.js

---
+++ RAT.js
@@ -0,0 +1,9 @@
+// var http = require('http');
+
+// TEST
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+console.log('Hello World!')
